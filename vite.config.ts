5cd5d24edfdb9d9cc505f2a41c340d4763fd9c04
dvirberlo <dvir.berl@gmail.com>
2022-11-16 18:55:24 +0000

    chore: configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,21 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico"],
+      manifest: {
+        name: "Psycho Clock",
+        short_name: "PsychoClock",
+        theme_color: "#000000",
+        icons: [],
+      },
+    }),
+  ],
 });
